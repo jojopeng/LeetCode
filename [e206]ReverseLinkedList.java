bc45a5164603ceb4abdc72957808b387e904af6b
jojopeng <jojopeng@users.noreply.github.com>
2019-04-01 23:57:57 +0800

    Create [e206]ReverseLinkedList.java

---
+++ [e206]ReverseLinkedList.java
@@ -0,0 +1,22 @@
+206. Reverse Linked List
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+//recursive
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
