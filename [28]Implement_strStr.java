ed1b020d900593f3cd0613d57a0f574d356e145d
jojopeng <jojopeng@users.noreply.github.com>
2019-04-06 23:57:26 +0800

    Create [28]Implement_strStr.java

---
+++ [28]Implement_strStr.java
@@ -0,0 +1,21 @@
+#28. Implement strStr()
+#Implement strStr().
+
+#Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+#Example 1:
+
+#Input: haystack = "hello", needle = "ll"
+#Output: 2
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+              if (j == needle.length()) return i;
+              if (i + j == haystack.length()) return -1;
+              if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+          }
+    }
+}
