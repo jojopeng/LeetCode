6ae5823ce893d4c0899188a33904dd47059a1825
jojopeng <jojopeng@users.noreply.github.com>
2019-04-07 00:20:26 +0800

    Update [28]Implement_strStr.java
    28 －》2 methods

--- [28]Implement_strStr.java
+++ [28]Implement_strStr.java
@@ -8,6 +8,7 @@
 #Input: haystack = "hello", needle = "ll"
 #Output: 2
 
+# method 1
 class Solution {
     public int strStr(String haystack, String needle) {
         for (int i = 0; ; i++) {
@@ -17,5 +18,20 @@ class Solution {
               if (needle.charAt(j) != haystack.charAt(i + j)) break;
             }
           }
+    }
+}
+
+
+#method 2
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0) return 0;
+        if(needle.length()>haystack.length()) return -1;
+        if(needle.length()==haystack.length()&&needle.equals(haystack)) return 0;
+        for(int i=0;i<=haystack.length()-needle.length();i++){    //注意边界等于的细节
+            if(haystack.substring(i,i+needle.length()).equals(needle))
+                return i;
+        }
+        return -1;
     }
 }
