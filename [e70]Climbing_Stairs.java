c8c1ac679dd590ac9f279bbb7be4cd921d678edd
jojopeng <jojopeng@users.noreply.github.com>
2019-04-16 23:44:53 +0800

    Update [e70]Climbing_Stairs.java
    爬梯子，动态规划，裴波那契

--- [e70]Climbing_Stairs.java
+++ [e70]Climbing_Stairs.java
@@ -14,7 +14,7 @@ Explanation: There are two ways to climb to the top.
 1. 1 step + 1 step
 2. 2 steps
 
-动态规划
+method1 -> 动态规划
 */
 
 class Solution {
@@ -29,5 +29,24 @@ class Solution {
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
+    }
+}
+
+/*
+method2 -> Fibonacci Number
+*/
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
     }
 }
