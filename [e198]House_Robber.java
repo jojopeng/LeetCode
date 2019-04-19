0d93d32b0e38b6aee320c93cab7a517afd1f9fce
jojopeng <jojopeng@users.noreply.github.com>
2019-04-20 00:00:48 +0800

    Update and rename [198]House_Robber.java to [e198]House_Robber.java

--- [e198]House_Robber.java
+++ [e198]House_Robber.java
@@ -16,8 +16,8 @@ Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 
 class Solution {
     public int rob(int[] nums) {
-        int rob = 0; 
-        int notrob = 0; 
+        int rob = 0;   //max monney can get if rob current house
+        int notrob = 0; //max money can get if not rob current house
         for(int i=0; i<nums.length; i++) {
             int currob = notrob + nums[i]; //if rob current value, previous house must not be robbed
             notrob = Math.max(notrob, rob); 
@@ -26,3 +26,19 @@ class Solution {
         return Math.max(rob, notrob);
     }
 }
+
+
+/*
+public int rob(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        int[] dp = {nums[0], Math.max(nums[0], nums[1])};
+        int index=0;
+        for(int i=2; i<nums.length; i++){
+            index = i&1; // i&1 === i%2.  //index^1: 0->1, 1->0.
+        	dp[index] = Math.max(nums[i]+dp[index], dp[index^1]); 
+        }
+		return Math.max(dp[0], dp[1]);
+	}
+
+*/
