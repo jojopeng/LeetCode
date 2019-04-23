64ea7e0220813b0721681ebb85646c961735be3a
jojopeng <jojopeng@users.noreply.github.com>
2019-04-23 23:54:30 +0800

    Update [e122]Best_Time_to_Buy_and_Sell_Stock_II.java

--- [e122]Best_Time_to_Buy_and_Sell_Stock_II.java
+++ [e122]Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -13,3 +13,18 @@ Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-
              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 */
 
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, i = 0;        
+        while (i < prices.length) {
+            // find next local minimum
+            while (i < prices.length-1 && prices[i+1] <= prices[i]) i++;
+            int min = prices[i++]; // need increment to avoid infinite loop for "[1]"
+            // find next local maximum
+            while (i < prices.length-1 && prices[i+1] >= prices[i]) i++;
+            profit += i < prices.length ? prices[i++] - min : 0;
+        }
+        return profit;
+    }
+}
+
