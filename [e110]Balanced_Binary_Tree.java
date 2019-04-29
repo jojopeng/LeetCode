690a39a5226cfda2def6398639970f1559e44bbf
jojopeng <jojopeng@users.noreply.github.com>
2019-04-29 23:12:20 +0800

    Update [e110]Balanced_Binary_Tree.java

--- [e110]Balanced_Binary_Tree.java
+++ [e110]Balanced_Binary_Tree.java
@@ -50,3 +50,21 @@ class Solution {
 2. the bottom up way
 */
 
+class Solution {
+    public int dfsHeight(TreeNode root){
+        if (root == null) return 0;
+        
+        int leftHeight = dfsHeight (root.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = dfsHeight (root.right);
+        if (rightHeight == -1) return -1;
+        
+        if (Math.abs(leftHeight - rightHeight) > 1)  return -1;
+        return Math.max (leftHeight, rightHeight) + 1;
+    }
+    
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight (root) != -1;
+    }
+}
+
