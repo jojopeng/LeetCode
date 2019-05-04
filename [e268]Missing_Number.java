622aede833c8a3fff5b90f9ab47b57fffa629a0b
jojopeng <jojopeng@users.noreply.github.com>
2019-05-04 23:58:12 +0800

    Create [e268]Missing_Number.java

---
+++ [e268]Missing_Number.java
@@ -0,0 +1,21 @@
+/*
+268. Missing Number
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+//二进制方法
+*/
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+
+        return xor ^ i;
+    }
+}
